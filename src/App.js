7ba0c9da34228b5d9a88a49b6b04028b16347617
Ryan <ryanunroe@gmail.com>
2020-08-20 12:49:40 -0600

    Removed example files and stubbed out shell of route pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, isValidElement} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import e from 'express';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.changeKey = this.changeKey.bind(this);
+
+    this.state = {
+        key: ''
+    };
+  }
+  changeKey = (value) => {
+    this.setState({
+        key: value
+    });
+  }
+  isValid = key => {
+    getData = () => {
+      fetch(`http://localhost:3001/validateKey?key=${this.state.key}`)
+      .then(response => {
+          return response.json();
+      }).then(data => {
+          console.log(data);
+          return data;
+      });
+  }
+  }
+  render() {
+    if(isValid(this.state.key)) {
+      return (
+        <Router>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/signup">Sgfdg</Link>
+            <Link to="/cart">Cart</Link>
+          </nav>
+        </Router>
+      );
+    }
+    else {
+      return (
+        <Router>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Log In</Link>
+          </nav>
+        </Router>
+      );
+    }
+  }
 }
 
 export default App;
